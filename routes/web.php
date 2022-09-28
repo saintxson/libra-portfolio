2e24950d0cf44cce2e0389e282f8eb73aadb53f9
stikson <85546685+saintxson@users.noreply.github.com>
2022-09-28 15:44:04 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Book\IndexController');
 
 Route::group(['namespace' => 'Book', 'prefix' => 'books'], function () {
     Route::get('/', 'IndexController')->name('book.index');
